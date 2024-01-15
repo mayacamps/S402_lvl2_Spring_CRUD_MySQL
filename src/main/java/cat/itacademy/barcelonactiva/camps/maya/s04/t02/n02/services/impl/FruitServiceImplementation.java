bcb3429cd565800c88592970923ebd23c80587cc
Maya <maya.camps@estudiantat.upc.edu>
2024-01-15 11:10:04 +0100

    files uploaded from S402_lvl1

---
+++ src/main/java/cat/itacademy/barcelonactiva/camps/maya/s04/t02/n02/services/impl/FruitServiceImplementation.java
@@ -0,0 +1,67 @@
+package cat.itacademy.barcelonactiva.camps.maya.s04.t02.n02.services.impl;
+
+import cat.itacademy.barcelonactiva.camps.maya.s04.t02.n02.domain.Fruit;
+import cat.itacademy.barcelonactiva.camps.maya.s04.t02.n02.repository.FruitRepository;
+import cat.itacademy.barcelonactiva.camps.maya.s04.t02.n02.services.FruitService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class FruitServiceImplementation implements FruitService {
+
+    @Autowired
+    FruitRepository fruitRepo;
+
+    @Override
+    public List<Fruit> getAllFruits() {
+        return new ArrayList<Fruit>(fruitRepo.findAll());
+    }
+
+    @Override
+    public Fruit getFruit(int id) {
+        Optional<Fruit> fruit = fruitRepo.findById(id);
+        return fruit.orElse(null);
+    }
+
+    @Override
+    public void addFruit(Fruit fruit) {
+        fruitRepo.save(fruit);
+    }
+
+    @Override
+    public boolean updateFruit(int id, Fruit fruit) {
+        Optional<Fruit> oldFruit = fruitRepo.findById(id);
+        if (oldFruit.isPresent()){
+            boolean isChanged = false;
+            Fruit updatedFruit = oldFruit.get();
+            if (fruit.getName() != null && !fruit.getName().equalsIgnoreCase(oldFruit.get().getName())){
+                updatedFruit.setName(fruit.getName());
+                isChanged = true;
+            }
+            if (fruit.getQuantKilos() != null && fruit.getQuantKilos() != oldFruit.get().getQuantKilos()){
+                updatedFruit.setQuantKilos(fruit.getQuantKilos());
+                isChanged = true;
+            }
+            fruitRepo.save(updatedFruit);
+            return isChanged;
+        } else{
+            throw new EntityNotFoundException();
+        }
+    }
+
+    @Override
+    public void deleteFruit(int id) {
+        Optional<Fruit> fruitToDelete = fruitRepo.findById(id);
+        if (fruitToDelete.isPresent()) {
+            fruitRepo.deleteById(id);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+}
